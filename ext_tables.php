b3837198d5ef341ebbd3431fc933473054dce739
jonathanheilmann <mail@jonathan-heilmann.de>
2016-06-18 20:45:37 +0200

    [TASK] Remove unused TCA fields (Resolves #8)

--- ext_tables.php
+++ ext_tables.php
@@ -18,6 +18,9 @@ $pluginSignature = strtolower($extensionName) . '_pi1';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:'.$_EXTKEY.'/Configuration/FlexForms/pi1.xml');
 
+// Removed unused fields
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'layout,select_key,pages,recursive';
+
 // Add special mfp palette
 $GLOBALS['TCA']['sys_file_reference']['palettes']['jhPhotoswipePalette'] = array(
     'showitem' => 'title, author;;;;3-3-3,--linebreak--, description',
