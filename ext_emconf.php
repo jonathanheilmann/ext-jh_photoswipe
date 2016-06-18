9ce1dc8d7299a1e74e9c398baf53dd35941489c3
jonathanheilmann <mail@jonathan-heilmann.de>
2016-06-18 21:30:04 +0200

    [RELEASE] Release of version 0.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '0.0.3-dev',
+  'version' => '0.0.3',
   'constraints' => 
   array (
     'depends' => 
