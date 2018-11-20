3204e5403de6eed567bf62eea82f36b8f914257b
Jonathan Heilmann <mail@jonathan-heilmann.de>
2018-11-20 20:54:11 +0100

    [TASK] Set version to 1.0.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
   'uploadfolder' => false,
   'createDirs' => '',
   'clearCacheOnLoad' => 0,
-  'version' => '0.3.0',
+  'version' => '1.0.0-dev',
   'constraints' => 
   array (
     'depends' => 
